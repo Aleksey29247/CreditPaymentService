4e652e2cb413016da36eb0c76ff5006ef540da1b
Aleksey29247 <alik29247@gmail.com>
2022-01-04 16:59:00 +0700

    Test

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,17 +1,11 @@
 import java.util.regex.Matcher;
 
 public class CreditPaymentService {
-    private double _yeadstat;
-    private double k;
-    public CreditPaymentService(){
-        _yeadstat=0;
-        k=0;
-    }
-    public int calculate(float percent,int month, int credit)
-    {
-        _yeadstat=percent/12/100;
-        k=_yeadstat* Math.pow((1+_yeadstat),month)/(Math.pow((1+_yeadstat),month)-1);
-        return (int)(k*credit);
+
+    public int calculate(float percent, int month, int credit) {
+        double _yeadstat = percent / 12 / 100;
+        double k = _yeadstat * Math.pow((1 + _yeadstat), month) / (Math.pow((1 + _yeadstat), month) - 1);
+        return (int) (k * credit);
     }
 
 
